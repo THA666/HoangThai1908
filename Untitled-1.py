4a85cdd1dd2c02231cb2be4818b3900ed7fd3038
Hoang Thai <ytuthai@gmail.com>
2026-04-17 09:04:05 +0700

    Python test

---
+++ Untitled-1.py
@@ -0,0 +1,33 @@
+print("    ")
+print("* * * * *")
+print("* * * * *")
+print("* * * * *")
+print("* * * * *")
+print('\n')
+
+print('* * * * *')
+print('* * * * *')
+print('* * * * *')
+print('\n')
+print('* * * * *')
+print('*       *')
+print('* * * * *')
+print('\n')
+print('  * * ')
+print(' *   * ')
+print(' *   * ')
+print(' *   * ')
+print('  * * ')
+print('\n')
+print('* * * * *')
+print('  * * * ')
+print('  * * * ')
+print('  * * * ')
+print('* * * * *')
+print('\n')
+
+
+print("""Quê hương là chùm khế ngọt
+Cho con trèo hái mỗi ngày
+Quê hương là đường đi học
+Con về rợp bướm vàng bay""")
