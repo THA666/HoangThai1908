5f1c30794c7edb57e5cdcb279963b3f11a08c55b
Hoang Thai <ytuthai@gmail.com>
2026-04-24 09:53:45 +0700

    Lambda trả về giá trị của n+15

--- Untitled-13.py
+++ Untitled-13.py
@@ -1,4 +1,4 @@
-# cau_a.py
+# cau_a.tri tuyet doi cua mot so
 abs_val = lambda n: abs(n)
 
 n = int(input("Nhập số nguyên n: "))
---
+++ Untitled-14.py
@@ -0,0 +1,5 @@
+# cau_b.tra ve gia tri cua n+15
+add_15 = lambda n: n + 15
+
+n = int(input("Nhập số nguyên n: "))
+print(f"Kết quả {n} + 15 là: {add_15(n)}")
