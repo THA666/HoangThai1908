1a83d2fa8178496fe01e44a21830b6d5c0ec6028
Hoang Thai <ytuthai@gmail.com>
2026-04-24 09:55:58 +0700

    Lambda trả về tích của x và y

---
+++ Untitled-15.py
@@ -0,0 +1,6 @@
+# cau_c.tra tích cau x va y
+multiply = lambda x, y: x * y
+
+x = int(input("Nhập số thứ nhất (x): "))
+y = int(input("Nhập số thứ hai (y): "))
+print(f"Tích của {x} và {y} là: {multiply(x, y)}")
